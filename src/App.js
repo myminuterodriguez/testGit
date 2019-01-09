1e413e0b489125d7c133be63649784c17f924bd0
root <e.rodriguez@myminutetrip.com>
2019-01-09 14:54:11 +0100

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  FirstComponent from './FirstComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +19,10 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React with Eric
           </a>
         </header>
+        <FirstComponent/>
       </div>
     );
   }
---
+++ src/FirstComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="FirstComponent">
+        <h1>My first component</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et perferendis in illum, ut nulla animi deleniti neque facilis consequuntur eum error exercitationem, quaerat quibusdam distinctio magnam saepe enim quod aliquam!</p>
+      </div>
+    );
+  }
+}
+
+export default FirstComponent;
